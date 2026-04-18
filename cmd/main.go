5b3bb73c44e4dfd1c5da5e340274e6e0d84a5bcc
Gasser-Hassan <hassag2@mcmaster.ca>
2026-04-18 15:03:14 -0400

    working server and database

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,4 @@
 package main
-
 import (
 	"database/sql"
 	"encoding/json"
@@ -9,6 +8,7 @@ import (
 
 	_ "github.com/lib/pq"
 )
+var db *sql.DB
 type Job struct {
 	ID   int
 	Type string
@@ -51,11 +51,50 @@ func logTrace(db *sql.DB, jobID int, step int, message string) {
 		jobID, step, message,
 	)
 }
+func submitJob(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Println("📩 Received request")
+
+		var data struct {
+			Type string `json:"type"`
+			Data []int  `json:"data"`
+		}
+
+		err := json.NewDecoder(r.Body).Decode(&data)
+		if err != nil {
+			http.Error(w, "Invalid request", http.StatusBadRequest)
+			return
+		}
+
+		var jobID int
+		err = db.QueryRow(
+			"INSERT INTO jobs (type, status) VALUES ($1, 'PENDING') RETURNING id",
+			data.Type,
+		).Scan(&jobID)
+
+		if err != nil {
+			fmt.Println("DB ERROR:", err)
+			http.Error(w, "DB error", 500)
+			return
+		}
+
+		jobQueue <- Job{
+			ID:   jobID,
+			Type: data.Type,
+			Data: data.Data,
+		}
+
+		fmt.Println("✅ Job queued:", jobID)
+
+		json.NewEncoder(w).Encode(map[string]int{"job_id": jobID})
+	}
+}
 // queue (channel)
 var jobQueue = make(chan Job, 100)
 func main() {
 	connStr := "user=gasser dbname=job_engine sslmode=disable"
-
+	var err error
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
@@ -85,12 +124,15 @@ func main() {
 	}
 
 	// send job into queue
-	jobQueue <- Job{
-		ID:   jobID,
-		Type: "TEST",
-		Data: []int{10, 20, 30},
-	}
+	for i := 0; i < 3; i++ {
+	go worker(i, db)
+}
 
-	// 🔴 KEEP PROGRAM RUNNING (NOW THIS IS VALID)
-	time.Sleep(5 * time.Second)
+// setup API route
+http.HandleFunc("/jobs", submitJob(db))
+
+
+
+fmt.Println("🚀 Server running on http://localhost:8081")
+log.Fatal(http.ListenAndServe(":8081", nil))
 }
